a1cd54ec95d2602863a977537f45dbf6f1851fe3
jacobodavidson <jacobodavidson@outlook.com>
2025-03-22 21:38:59 -0400

    Initial commit

---
+++ agent.py
@@ -0,0 +1,9 @@
+import os
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from dotenv import load_dotenv
+
+# Environment variables
+load_dotenv()
